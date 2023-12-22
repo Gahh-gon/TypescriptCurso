75bb4453191833844f08d1d464036af03e581469
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2023-12-22 15:15:13 -0300

    Aula 10

---
+++ build/aula10.js
@@ -0,0 +1,9 @@
+"use strict";
+let verNome;
+verNome = null;
+console.log(verNome);
+let verNome2;
+console.log(verNome2);
+let verNome3 = 10;
+let Vnome;
+console.log(verNome3);
---
+++ src/aula10.ts
@@ -0,0 +1,17 @@
+// null -undefined - unknown
+// nulo indefinido desconhecido
+
+// null - tipo nulo
+let verNome:null|string;
+verNome = null
+console.log(verNome)
+
+// undefined - indefinido 
+let verNome2:any;
+console.log(verNome2) 
+
+let verNome3:unknown = 10;
+let Vnome:number;
+// Vnome = verNome3
+console.log(verNome3)
+// console.log(Vnome)
