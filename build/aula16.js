ecc37e8b0eaf74eb45f2592f93edcbeedcfe5244
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2023-12-30 20:41:18 -0300

    aula 16

---
+++ build/aula16.js
@@ -0,0 +1,16 @@
+"use strict";
+class computador {
+    nome = '';
+    mem = 0;
+    cpu = 0;
+    ligado = false;
+}
+const comp1 = new computador();
+const comp2 = new computador();
+const comp3 = new computador();
+comp1.nome = 'Rapidão';
+comp2.nome = 'gloria';
+comp3.nome = 'gamer';
+console.log(comp1.nome);
+console.log(comp2.nome);
+console.log(comp3.nome);
